46c541617d66a2aa14a8d4ec8a3b6628e52693f5
alejandrochaguay <146053412+alejandrochaguay@users.noreply.github.com>
2023-12-26 22:01:27 -0500

    concatenar

---
+++ script/Concatenar dos cadenas.js
@@ -0,0 +1,9 @@
+function concatenar(){
+    var cadena1 
+    var cadena2 
+    cadena1 = document.getElementById("cadena1").value
+    cadena2 = document.getElementById("cadena2").value
+    var resultado = cadena1 + cadena2;
+    Mensaje(resultado);
+}
+
