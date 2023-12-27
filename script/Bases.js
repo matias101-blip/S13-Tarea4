b6929e26651a018a11e6c8169f03c00a67a47be1
Jennerst <148028925+Jennerst@users.noreply.github.com>
2023-12-27 03:40:08 -0500

    ahora si

--- script/Bases.js
+++ script/Bases.js
@@ -2,34 +2,40 @@ function conver1() {
     let num = document.getElementById("num").value;
     let Base2 = parseInt(num, 10).toString(2);
     alert(`El número ${num} en base 2 es ${Base2}`);
+    document.getElementById("num").value = "";
 }
 
 function conver2() {
     let num2 = document.getElementById("num2").value;
     let Base8 = parseInt(num2, 10).toString(8);
     alert(`El número ${num2} en base 8 es ${Base8}`);
+    document.getElementById("num2").value = "";
 }
 
 function conver3() {
     let num3 = document.getElementById("num3").value;
     let Base16 = parseInt(num3, 10).toString(16);
     alert(`El número ${num3} en base 16 es ${Base16}`);
+    document.getElementById("num3").value = "";
 }
 
 function conver4() {
     let num4 = document.getElementById("num4").value;
     let Base10 = parseInt(num4, 2).toString(10);
     alert(`El número ${num4} en base 10 es ${Base10}`);
+    document.getElementById("num4").value = "";
 }
 
 function conver5() {
     let num5 = document.getElementById("num5").value;
     let Base8_2 = parseInt(num5, 2).toString(8);
     alert(`El número ${num5} en base 8 es ${Base8_2}`);
+    document.getElementById("num5").value = "";
 }
 
 function conver6() {
     let num6 = document.getElementById("num6").value;
     let Base16_2 = parseInt(num6, 2).toString(16);
     alert(`El número ${num6} en base 16 es ${Base16_2}`);
+    document.getElementById("num6").value = "";
 }
